f1f3640e63ed97b8b7c0fae3937632a0f0f40618
Francisco Giraldo <fgiraldo@wearenolte.com>
2020-06-25 14:13:58 -0500

    New design

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # franciscogiraldo.com
 Personal Website
+
+##build
+npx tailwindcss build styles.css -o build.css
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html'],
+  theme: {
+  	container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
