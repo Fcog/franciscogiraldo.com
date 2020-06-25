41cefa6f86a5f08cf6d2ad074cc72903a51e85d8
Francisco Giraldo <fgiraldo@wearenolte.com>
2020-06-25 16:44:46 -0500

    New design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: ['./index.html'],
   theme: {
+  	colors: {
+  		white: '#ffffff',
+  		black: '#000000',
+  		blue: '#59a4ff',
+  		'dark-blue': '#024fa9',
+  	},
   	container: {
       center: true,
     },
