91dff6c89c74822d3ccd6999862fa191fec7f134
Francisco Giraldo <fgiraldo@wearenolte.com>
2020-06-28 14:51:54 -0500

    Added offline service worker

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{svg,webmanifest,webm,html,js,css}"
+  ],
+  "swDest": "dist/sw.js",
+  "swSrc": "service-worker.js"
+};
