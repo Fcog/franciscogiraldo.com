b0ea4fa3a51941dac2ecc7dbfe0c5bb5b4f9201a
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2021-04-06 09:50:43 -0500

    Next prev links now take into account posts status

--- .eleventy.js
+++ .eleventy.js
@@ -9,28 +9,28 @@ module.exports = (config) => {
 
     config.addCollection("posts", function(collection) {
         const posts_collection = collection.getFilteredByTag("post");
-      
-        /** 
-         * Post's Next Prev links configuration.
-         * 
-         * Adds the previous and next links of posts on a current post.
-         */
-        for (let i = 0; i < posts_collection.length ; i++) {
-          const prevPost = posts_collection[i - 1];
-          const nextPost = posts_collection[i + 1];
-      
-          posts_collection[i].data["prevPost"] = prevPost;
-          posts_collection[i].data["nextPost"] = nextPost;
-        }
 
         /**
          * Post's draft status configuration.
          * 
          * Any post with the draft status will be removed from the collection.
          */
-        const publishedPosts = (post) => post.data.status !== 'draft';
+        const publishedPosts = posts_collection.filter( post => post.data.status !== 'draft' );
       
-        return posts_collection.filter(publishedPosts);
+        /** 
+         * Post's Next Prev links configuration.
+         * 
+         * Adds the previous and next links of posts on a current post.
+         */
+        for (let i = 0; i < publishedPosts.length ; i++) {
+          const prevPost = publishedPosts[i - 1];
+          const nextPost = publishedPosts[i + 1];
+      
+          publishedPosts[i].data["prevPost"] = prevPost;
+          publishedPosts[i].data["nextPost"] = nextPost;
+        }
+      
+        return publishedPosts;
     });
 
     return {
